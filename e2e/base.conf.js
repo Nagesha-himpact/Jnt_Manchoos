58ce3416854b6e2371456b3b62e4e81e6202531a
Nagesh_himpact <72196803+Nagesha-himpact@users.noreply.github.com>
2021-03-19 19:12:30 +0530

    change

--- e2e/base.conf.js
+++ e2e/base.conf.js
@@ -6,8 +6,8 @@ var fs = require('fs-extra');
 waitTimeout = 120000;
 
 config.specs = [
-    'global.js',
-	'specs/sampleTest.js',
+    './global.js',
+	'./specs/sampleTest.js',
 	//'specs/manchoos-Valet.js',
 	//'specs/manchoos-DineIn.js',
 	//'specs/manchoos-Tmgs-Calendar.js'
