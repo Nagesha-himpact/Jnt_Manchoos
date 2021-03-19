603489b1c26bd6d9a4bd53b8ffe6432364b9e997
Nagesh_himpact <72196803+Nagesha-himpact@users.noreply.github.com>
2021-03-19 18:35:23 +0530

    Update addOnsPage.js

--- e2e/pages/addOnsPage.js
+++ e2e/pages/addOnsPage.js
@@ -35,6 +35,8 @@ module.exports = function () {
         browser.get(path)
         return this;
     }
+    
+    
 
     this.clickOnHeaderAddOns=function(){
         buttonActions.click(headerAddOns)
